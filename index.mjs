9f05427d4d5925b99b777fefc0985a2621e15f0e
Larysa Rud <larpavrud@gmail.com>
2024-05-24 22:05:28 -0400

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+// Configuration to read our enviroment variables
+dotenv.config();
+import grades from './routes/grades.mjs';
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+//Connect to DB
+// import './db/conn.mjs';
+
+// JSON middleware
+app.use(express.json());
+
+// Grades Routes
+app.use('/api/grades', grades);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Grades API.');
+});
+
+//Global Error Handling Middlware
+app.use((err, req, res, next) => {
+  res.status(500).send('Seems like we messed up somewhere...');
+});
+
+// Start the Express Server
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
---
+++ routes/grades.mjs
@@ -0,0 +1,19 @@
+import express from 'express';
+import db from '../db/conn.mjs';
+import { ObjectId } from 'mongodb';
+
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) {
+    res.send('Not Found').status(400);
+  } else {
+    res.send(result).status(200);
+  }
+});
+
+export default router;
